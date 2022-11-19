96f62d631005bb18e41292b6d4b4abfae121667e
carver <1048426439@qq.com>
2022-11-19 17:03:18 +0800

    登录注册创建的UI逻辑

---
+++ Src/Client/Assets/Scripts/LoadingManager.cs
@@ -0,0 +1,53 @@
+﻿using Services;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class LoadingManager : MonoBehaviour {
+
+	public GameObject UITips;
+	public GameObject UILoading;
+	public GameObject UILogin;
+
+	public Slider progressBar;
+	public Text progressNumber;
+
+	// Use this for initialization
+	IEnumerator Start () {
+		log4net.Config.XmlConfigurator.ConfigureAndWatch(new System.IO.FileInfo("log4net.xml"));
+		UnityLogger.Init();
+		Common.Log.Init("Unity");
+		Common.Log.Info("LoadingManager start");
+
+		UITips.SetActive(true);
+		UILoading.SetActive(false);
+		UILogin.SetActive(false);
+		yield return new WaitForSeconds(2f);
+		UILoading.SetActive(true);
+		yield return new WaitForSeconds(1f);
+		UITips.SetActive(false);
+
+		yield return DataManager.Instance.LoadData();
+
+		UserService.Instance.Init();
+
+		for (float i = 50; i < 100;)
+        {
+			i += Random.Range(0.1f, 1.5f);
+			progressBar.value = i;
+			progressNumber.text = progressBar.value.ToString() + "%";
+			yield return new WaitForEndOfFrame();
+        }
+
+		UILoading.SetActive(false);
+		UILogin.SetActive(true);
+		yield return null;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Src/Client/Assets/Scripts/UI/UILogin.cs
@@ -0,0 +1,50 @@
+﻿using Services;
+using SkillBridge.Message;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UILogin : MonoBehaviour {
+
+	public InputField username;
+	public InputField password;
+	public Button buttonLogin;
+
+	// Use this for initialization
+	void Start () {
+		UserService.Instance.OnLogin = OnLogin;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnClickLogin()
+    {
+		if (string.IsNullOrEmpty(this.username.text))
+        {
+			MessageBox.Show("请输入账号");
+			return;
+        }
+		if (string.IsNullOrEmpty(this.password.text))
+        {
+			MessageBox.Show("请输入密码");
+			return;
+        }
+		UserService.Instance.SendLogin(this.username.text, this.password.text);
+    }
+
+	void OnLogin(Result result, string message)
+    {
+		if (result == Result.Success)
+        {
+			SceneManager.Instance.LoadScene("CharSelect");
+        }
+		else
+        {
+			MessageBox.Show(message, "错误", MessageBoxType.Error);
+        }
+    }
+}
